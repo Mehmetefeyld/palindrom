95d503bacf40a92ce2cd0f3324d45005c6df4c6a
Mehmetefeyld <mefeyld@gmail.com>
2023-10-17 12:03:45 +0300

    c# palindrom
    c# da girilen kelimenin palindrom kelime olup olmadığını kontrol eden program

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class ATM
+{
+    static double accountBalance = 1000;
+
+    static void Main()
+    {
+
+
+        Console.Write("Bir kelime giriniz: ");
+        string word = Console.ReadLine();
+
+        if (IsPalindrome(word))
+        {
+            Console.WriteLine(word + "     palindrom!");
+        }
+        else
+        {
+            Console.WriteLine(word + "    palindrom DEĞİL!");
+        }
+
+        Console.ReadKey();
+    }
+
+    static bool IsPalindrome(string word)
+    {
+        int length = word.Length;
+
+        for (int i = 0; i < length / 2; i++)
+        {
+            if (word[i] != word[length - 1 - i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
